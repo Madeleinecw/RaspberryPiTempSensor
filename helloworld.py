5323829627841999c2c95f3aa163376613bb11a5
Madeleinecw <madeleinecwood@gmail.com>
2021-03-17 16:17:07 +0000

    added template

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,15 @@
 import time
 from w1thermsensor import W1ThermSensor
 from datetime import datetime
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+if __name__ == '__main__':
+    socketio.run(app)
 
 sensor = W1ThermSensor() 
 
@@ -10,4 +17,4 @@ sensor = W1ThermSensor()
 def hello_world():
     temperature = sensor.get_temperature()
     timeNow = datetime.now().time().replace(microsecond=0)
-    return"At %s the temperature is %s celsius" % (timeNow, temperature)
+    return render_template('hello.html', temperature=temperature, timeNow=timeNow) 
