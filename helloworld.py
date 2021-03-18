8cbb4106a4a9ffcc6f60d56ee393d9149b0d5345
Madeleinecw <madeleinecwood@gmail.com>
2021-03-18 11:16:02 +0000

    added application.js to static, added style.css, removed js scrpit from base

--- helloworld.py
+++ helloworld.py
@@ -2,7 +2,7 @@ import time
 from w1thermsensor import W1ThermSensor
 from datetime import datetime
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -11,10 +11,15 @@ socketio = SocketIO(app)
 if __name__ == '__main__':
     socketio.run(app)
 
+@socketio.on('my event')
+def test_message(message):
+        emit('my response', {'data': 'got it!'})
+
 sensor = W1ThermSensor() 
+
 
 @app.route('/')
 def hello_world():
     temperature = sensor.get_temperature()
     timeNow = datetime.now().time().replace(microsecond=0)
-    return render_template('hello.html', temperature=temperature, timeNow=timeNow) 
+    return render_template('base.html', temperature=temperature, timeNow=timeNow) 
---
+++ static/application.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    var socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
+    socket.on('my response', function(msg) {
+        $('#log').append('<p>Received:' + msg.data + '</p>');
+    });
+    $('form#emit').submit(function(event){
+        socket.emit('my event', {data: $('#emit_data').val()});
+        return false;
+    });
+});
