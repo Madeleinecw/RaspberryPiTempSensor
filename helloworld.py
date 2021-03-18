96fe0a322f8cec4db1b6a89889bf3d0ec32912d4
Madeleinecw <madeleinecwood@gmail.com>
2021-03-18 18:06:43 +0000

    successfully updates time and temp to browser

--- helloworld.py
+++ helloworld.py
@@ -1,25 +1,54 @@
-import time
-from w1thermsensor import W1ThermSensor
-from datetime import datetime
-from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, url_for, copy_current_request_context
+from random import random
+from time import sleep
+from threading import Thread, Event
+from datetime import datetime 
+from w1thermsensor import W1ThermSensor
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+app.config['DEBUG'] = True
+
+socketio = SocketIO(app, async_mode=None, logger=True, engineio_logger=True)
+
+sensor = W1ThermSensor() 
+thread= Thread()
+thread_stop_event = Event()
+
+
+def send_time_and_temperature():
+    print("getting the temperature")
+    while not thread_stop_event.isSet():
+        temperature = sensor.get_temperature()
+        timeNow = datetime.now().time().replace(microsecond=0)
+        time = str(timeNow)
+        print(time)
+        print(temperature)
+        socketio.emit('newTemperature', {'temperature' : temperature}, namespace='/test')
+        socketio.emit('newTime', {'time' : time}, namespace='/test')
+        socketio.sleep(1)
+
+@app.route('/')
+def index():
+    #only by sending this page first will the client be connected to the socketio instance
+    return render_template('base.html')
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    # need visibility of the global thread object
+    global thread
+    print('Client connected')
+
+    #Start the random number generator thread only if the thread has not been started before.
+    if not thread.isAlive():
+        print("Starting Thread")
+        thread = socketio.start_background_task(send_time_and_temperature)
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected')
 
 if __name__ == '__main__':
     socketio.run(app)
-
-@socketio.on('my event')
-def test_message(message):
-        emit('my response', {'data': 'got it!'})
-
-sensor = W1ThermSensor() 
-
-
-@app.route('/')
-def hello_world():
-    temperature = sensor.get_temperature()
-    timeNow = datetime.now().time().replace(microsecond=0)
-    return render_template('base.html', temperature=temperature, timeNow=timeNow) 
--- static/application.js
+++ static/application.js
@@ -1,10 +1,38 @@
-$(document).ready(function(){
+$(document).ready(function() {
+
     var socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
-    socket.on('my response', function(msg) {
-        $('#log').append('<p>Received:' + msg.data + '</p>');
-    });
-    $('form#emit').submit(function(event){
-        socket.emit('my event', {data: $('#emit_data').val()});
-        return false;
-    });
-});
+    var temperature_received = [];
+    var time_received = [];
+
+    socket.on('newTemperature', function(msg) {
+        console.log('The Temperature is ' + msg.temperature)
+        
+        if (temperature_received.length >= 1) {
+            temperature_received.shift()
+        }
+
+        temperature_received.push(msg.temperature)
+        temperatures_string = '';
+
+        for (var i = 0; i < temperature_received.length; i++) {
+            temperatures_string = temperatures_string + '<p>' + temperature_received[i].toString() + '</p>';
+        }
+        $('#temp-log').html(temperatures_string);
+    })
+
+    socket.on('newTime', function(msg) {
+        console.log('The Time is ' + msg.time)
+
+        if (time_received.length >= 1) {
+            time_received.shift()
+        }
+
+        time_received.push(msg.time)
+        time_string = '';
+
+        for (var i = 0; i < time_received.length; i++) {
+            time_string = time_string + '<p>' + time_received[i].toString() + '</p>';
+        }
+        $('#time-log').html(time_string);
+    })
+})
