b76e6af3b00ef709d8e5d59b0b609264fe278b81
Madeleinecw <madeleinecwood@gmail.com>
2021-03-25 15:09:09 +0000

    added graph grid, ranges for x and y axes, started labeltooltip

--- testplotting.py
+++ testplotting.py
@@ -1,20 +1,29 @@
 from model.databasetemp import get_temps, get_timestamps
+from datetime import datetime, timedelta
 import matplotlib as mpl 
 mpl.use('Agg')
 from matplotlib import pyplot as plt
-from mpld3 import fig_to_html
+from mpld3 import fig_to_html, plugins
 
 def make_plot():
-    fig = plt.figure()
+ 
+    fig, ax = plt.subplots()
     dev_x = get_temps()
     dev_y = get_timestamps()
+
+    now = datetime.now()
+    yesterday = datetime.now() - timedelta(1)
     
+   
     plt.rcParams['axes.facecolor'] = '#621708'
     plt.rcParams['axes.titlesize'] = 24
     plt.rcParams['xtick.color'] = '#F6AA1C'
+    plt.rcParams['ytick.color'] = '#F6AA1C'
+    plt.grid(b=True, color="#AA280E")
 
-
-    plt.plot(dev_y, dev_x, color= '#F6AA1C')
+    plt.plot(dev_y, dev_x, 'o', color= '#F6AA1C')
+    plt.ylim(16, 20)
+    plt.xlim(yesterday, now)
 
     plt.title('Temperatures by Time of Day', color= '#F6AA1C')
     plt.ylabel('Temperatures', color= '#F6AA1C')
@@ -22,6 +31,14 @@ def make_plot():
 
     plt.show()
 
+    
+
     fig.savefig('static/temp.png')
+   
+    lines = ax.plot(dev_y, dev_x, color= '#F6AA1C')
+    labelTooltip = plugins.LineLabelTooltip(lines[0], label='ylabel')
+    plugins.connect(fig, labelTooltip)
     plt_html = fig_to_html(fig)
+
+
     return plt_html
